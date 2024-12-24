27567ce6889246c244f1ba1a0892666bdc9e6840
chuphal-11 <saurav230111002@gmail.com>
2024-12-25 01:11:32 +0530

    Update js.py

--- js.py
+++ js.py
@@ -1,15 +1,10 @@
 import requests
-import os
-import dotenv
-from dotenv import load_dotenv
 
-load_dotenv()
-api=os.getenv('API_KEY')
 
 def img(title,year):
     
     url_omdb = 'http://www.omdbapi.com/?t='+title+'&y='+year+'&apikey=974b4b9c'
-    url_imdb='https://api.themoviedb.org/3/search/movie?api_key='+api+'&query=Inception'
+    url_imdb='https://api.themoviedb.org/3/search/movie?api_key='+'a1600d56945f5a6533cff881c44f2654'+'&query=Inception'
     response = requests.get(url_omdb)
     a=response.json()
     try:
